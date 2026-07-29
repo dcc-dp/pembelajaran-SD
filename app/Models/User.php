29c5f17132d4e8a83744f119bddd84bf933ca211
alfinabun <alfinsyarif04@gmail.com>
2026-07-30 00:51:18 +0800

    feat: membuat tampilan login dan registrasi

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'nama_sekolah', 'email', 'no_hp', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
